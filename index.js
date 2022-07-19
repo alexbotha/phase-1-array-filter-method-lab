9c860650585e85eb48f9b4b50227fe5d97d15404
alexbotha <alexbotha@hotmail.co.uk>
2022-07-19 15:22:09 +0100

    commit tree 0dde969edc1c94d80db1952776d9a9c7e9745411

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arrayOfNames, string){
+let result = arrayOfNames.filter(driver => driver.toUpperCase() === string.toUpperCase())
+return result
+}
+
+function fuzzyMatch(arrayOfNames, string){
+  let result = arrayOfNames.filter(driver => driver.startsWith(string))
+  return result
+}
+
+function matchName(arrayofNames, string){
+  let result = arrayofNames.filter(driver => driver.name === string)
+  return result;
+}
+
